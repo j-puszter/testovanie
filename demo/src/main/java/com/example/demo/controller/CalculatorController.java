250e3bc736e0d426b6c6047f4136f2de52514866
xSinghofer <73109512+SingiSvk@users.noreply.github.com>
2022-12-13 12:42:42 +0100

    optimized import

--- demo/src/main/java/com/example/demo/controller/CalculatorController.java
+++ demo/src/main/java/com/example/demo/controller/CalculatorController.java
@@ -3,13 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.Calculator;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class CalculatorController {
-    
+
     @GetMapping("primes/{n}")
     public List<Integer> primeNumbers(@PathVariable Integer n) {
         return Calculator.primeNumbers(n);
