3a5df28b5b97c0cb6881a07d5d03af1ed034a22c
xSinghofer <73109512+SingiSvk@users.noreply.github.com>
2022-12-14 12:27:42 +0100

    ResponseEntity

--- demo/src/main/java/com/example/demo/controller/CalculatorController.java
+++ demo/src/main/java/com/example/demo/controller/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.Calculator;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,12 +12,16 @@ import java.util.List;
 public class CalculatorController {
 
     @GetMapping("/primes/{n}")
-    public static List<Integer> primeNumbers(@PathVariable Integer n) {
-        return Calculator.primeNumbers(n);
+    public static  ResponseEntity<List<Integer>> primeNumbers(@PathVariable Integer n) {
+        return ResponseEntity.ok(Calculator.primeNumbers(n));
     }
 
     @PostMapping("/isPrime")
-    public static boolean isPrime(@RequestBody Integer n) {
-        return Calculator.isPrime(n);
+    public static ResponseEntity<Boolean> isPrime(@RequestBody Integer n) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.AGE, "value");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(Calculator.isPrime(n));
     }
 }
