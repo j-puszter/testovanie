736242a7f28d6c04b32809838774e9ee2c929abb
j-puszter <j.puszter@gmail.com>
2022-12-14 21:34:26 +0100

    dopnenie unit testov do dokumentacie a mala uprava kodu

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-import static com.example.demo.Calculator.isPrime;
-
 
 @SpringBootApplication
 public class DemoApplication {
