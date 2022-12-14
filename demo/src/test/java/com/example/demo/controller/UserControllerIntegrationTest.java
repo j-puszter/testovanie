72918d7363821a40a5792d7058d54364a200badd
xSinghofer <73109512+SingiSvk@users.noreply.github.com>
2022-12-14 13:17:19 +0100

    Update UserControllerIntegrationTest + unit test example

--- demo/src/test/java/com/example/demo/controller/UserControllerIntegrationTest.java
+++ demo/src/test/java/com/example/demo/controller/UserControllerIntegrationTest.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import com.example.demo.DemoApplication;
-import com.example.demo.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,34 +13,37 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = DemoApplication.class)
 @AutoConfigureMockMvc
-//@TestPropertySource(
-  //      locations = "classpath:application-integrationtest.properties")
 class UserControllerIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
     @Autowired
-    private UserRepository repository;
+    private UserController controller;
 
     @Test
-    public void givenEmployees_whenGetEmployees_thenStatus200()
-            throws Exception {
-
-        //createTestEmployee("bob");
-
+    public void listUsers() throws Exception {
         mvc.perform(get("/user/list").contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-                //.andExpect(jsonPath("$[0].name", is("bob")));
-    }
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[5]").doesNotExist())
+                .andExpect(jsonPath("$[0].username").value("a"));
+    };
+
+
+    // Unit test
+
+    @Test
+    public void listUsers2() throws Exception {
+        Assertions.assertEquals(controller.list().getBody().size(), 5);
+    };
 
 }
